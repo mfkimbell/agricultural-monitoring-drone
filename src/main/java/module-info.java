4619412da32d1e90abea6063ded88630a63cca55
Kai Battle <kaiblaz3@uab.edu>
2023-10-19 21:29:08 -0500

    we ball

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.dronedashboard {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.dronedashboard to javafx.fxml;
-    exports com.example.dronedashboard;
-}
