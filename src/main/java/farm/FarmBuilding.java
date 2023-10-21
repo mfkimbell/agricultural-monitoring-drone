c3eaf3698d6c4ab85f8a25521191f0740c4b8bbf
Jayden Pyles <jpylesbusiness@gmail.com>
2023-10-21 14:17:24 -0500

    wip: fill out constructors for farm objects

--- src/main/java/farm/FarmBuilding.java
+++ src/main/java/farm/FarmBuilding.java
@@ -3,11 +3,15 @@ package farm;
 import java.util.ArrayList;
 
 public class FarmBuilding extends ItemContainer{
-    public FarmBuilding(String name, int x, int y) {
+    public FarmBuilding(String name, float price, float x, float y, float length, float width, float height) {
         this.name = name;
+        this.price = price;
         this.locationX = x;
         this.locationY = y;
         this.items = new ArrayList<Item>();
         this.itemContainers = new ArrayList<ItemContainer>();
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
 }
--- src/main/java/farm/FarmItem.java
+++ src/main/java/farm/FarmItem.java
@@ -1,8 +1,12 @@
 package farm;
 public class FarmItem extends Item {
-    public FarmItem(String name, int x, int y) {
+    public FarmItem(String name, float price, float x, float y, float length, float width, float height) {
         this.name = name;
+        this.price = price;
         this.locationX = x;
         this.locationY = y;
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
 }
