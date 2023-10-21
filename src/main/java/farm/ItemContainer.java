8c3613fa261ad7ec30d03b660dd448879188ed06
Jayden Pyles <jpylesbusiness@gmail.com>
2023-10-21 13:53:36 -0500

    wip: get item and container from item container

--- src/main/java/farm/ItemContainer.java
+++ src/main/java/farm/ItemContainer.java
@@ -1,8 +1,9 @@
 package farm;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
-public abstract class ItemContainer {
+public abstract class ItemContainer extends FarmObject {
     /* Vars */
     protected ArrayList<ItemContainer> itemContainers;
     protected ArrayList<Item> items;
@@ -21,5 +22,23 @@ public abstract class ItemContainer {
     public void deleteItemContainer(ItemContainer itemContainer){
         int index = itemContainers.indexOf(itemContainer);
         this.itemContainers.remove(index);
+    };
+
+    public Item getItemFromContainer(String name, int x, int y){
+        for (Item item: this.items){
+            if (Objects.equals(item.getName(), name) && item.getLocationX() == x && item.getLocationY() == y){
+                return item;
+            }
+        }
+        return null;
+    };
+
+    public ItemContainer getItemContainerFromContainer(String name, int x, int y){
+        for (ItemContainer container: this.itemContainers){
+            if (Objects.equals(container.getName(), name) && container.getLocationX() == x && container.getLocationY() == y){
+                return container;
+            }
+        }
+        return null;
     };
 }
