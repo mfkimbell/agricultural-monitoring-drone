d19aea769917fa844ea5bf8b563bc75b3e363188
Kai Battle <kaiblaz3@uab.edu>
2023-10-25 21:15:24 -0500

    Try compiling this bucko (Controller updates)

--- src/main/java/com/example/droneproject/DroneDashboardController.java
+++ src/main/java/com/example/droneproject/DroneDashboardController.java
@@ -5,10 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.*;
 
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -18,70 +20,141 @@ public class DroneDashboardController implements Initializable {
 
     public Object currentAction = "";
     public TreeItem<String> rootItem = new TreeItem<>();
-    public FarmItem newFarmItem = new FarmItem();
+    FarmItem newFarmItem = new FarmItem();
 
-    public FarmItem completeForms(Text prompt, TextField field, Dialog dialog){
-        AtomicReference<String> newItemName = new AtomicReference<>("");
-        AtomicReference<Float> newItemPrice = new AtomicReference(0);
-        AtomicReference<Float> newItemX = new AtomicReference(0);
-        AtomicReference<Float> newItemY = new AtomicReference(0);
-        AtomicReference<Float> newItemLength = new AtomicReference(0);
-        AtomicReference<Float> newItemWidth = new AtomicReference(0);
-        AtomicReference<Float> newItemHeight = new AtomicReference(0);
+    ArrayList <FarmObject> farmObjects = new ArrayList<>();
 
-
-        newItemName.set(field.getText());
-        field.clear();
-        prompt.setText("Enter Price Value");
-        newItemPrice.set(Float.parseFloat(field.getText().isEmpty() ? "0" : field.getText()));
-        field.clear();
-        prompt.setText("Enter X Value");
-        newItemX.set(Float.parseFloat(field.getText().isEmpty() ? "0" : field.getText()));
-        field.clear();
-        prompt.setText("Enter Y Value");
-        newItemY.set(Float.parseFloat(field.getText().isEmpty() ? "0" : field.getText()));
-        field.clear();
-        prompt.setText("Enter Length Value");
-        newItemLength.set(Float.parseFloat(field.getText().isEmpty() ? "0" : field.getText()));
-        field.clear();
-        prompt.setText("Enter Width Value");
-        newItemWidth.set(Float.parseFloat(field.getText().isEmpty() ? "0" : field.getText()));
-        field.clear();
-        prompt.setText("Enter Height Value");
-        newItemHeight.set(Float.parseFloat(field.getText().isEmpty() ? "0" : field.getText()));
-        FarmItem newItem = new FarmItem(
-                newItemName.get(),
-                newItemPrice.get(),
-                newItemX.get(),
-                newItemY.get(),
-                newItemLength.get(),
-                newItemWidth.get(),
-                newItemHeight.get());
-        return newItem;
-
-    }
     public void createItem(){
 
         Dialog<Boolean> newItemDialog = new Dialog();
         TextField itemNameField = new TextField();
+        TextField itemPriceField = new TextField();
+        TextField itemXField = new TextField();
+        TextField itemYField = new TextField();
+        TextField itemLengthField = new TextField();
+        TextField itemWidthField = new TextField();
+        TextField itemHeightField = new TextField();
+
         Button acceptButton = new Button("OK");
         newItemDialog.setResult(true);
-        Text txt = new Text("Enter Item Name");
+        Text txt = new Text("Enter Item Values");
         VBox pane = new VBox(10);
         AtomicReference<FarmItem> newItem = new <FarmItem>AtomicReference();
 
         newItemDialog.setTitle("New Item");
 
         acceptButton.setOnAction(e -> {
-           newItem.set(completeForms(txt, itemNameField, newItemDialog));
+            FarmItem values = new FarmItem(
+                    itemNameField.getText(),
+                    Float.parseFloat(itemPriceField.getText()),
+                    Float.parseFloat(itemXField.getText()),
+                    Float.parseFloat(itemYField.getText()),
+                    Float.parseFloat(itemLengthField.getText()),
+                    Float.parseFloat(itemWidthField.getText()),
+                    Float.parseFloat(itemHeightField.getText())
+            );
+            newItem.set(values);
+
+
            newItemDialog.close();
         });
         Font font = Font.font("verdana", FontWeight.MEDIUM, FontPosture.REGULAR, 12);
         txt.setFont(font);
+        Label nameLabel = new Label("Name");
+        Label priceLabel = new Label("Price");
+        Label xLabel = new Label("X Coordinate");
+        Label yLabel = new Label("Y Coordinate");
+        Label lengthLabel = new Label("Length");
+        Label widthLabel = new Label("Width");
+        Label heightLabel = new Label("Height");
 
-        pane.getChildren().addAll(txt, itemNameField, acceptButton);
+        GridPane fieldPane = new GridPane();
+        fieldPane.add(txt,0,0);
+        fieldPane.add(nameLabel, 0,1);
+        fieldPane.add(priceLabel, 0,2);
+        fieldPane.add(xLabel, 0,3);
+        fieldPane.add(yLabel, 0,4);
+        fieldPane.add(lengthLabel, 0,5);
+        fieldPane.add(widthLabel, 0,6);
+        fieldPane.add(heightLabel, 0,7);
+        fieldPane.add(itemNameField,1,1);
+        fieldPane.add(itemPriceField,1,2);
+        fieldPane.add(itemXField,1,3);
+        fieldPane.add(itemYField,1,4);
+        fieldPane.add(itemLengthField,1,5);
+        fieldPane.add(itemWidthField,1,6);
+        fieldPane.add(itemHeightField,1,7);
+        fieldPane.add(acceptButton, 0,9);
+        pane.getChildren().add(fieldPane);
         newItemDialog.getDialogPane().setContent(pane);
-        newItemDialog.show();
+        newItemDialog.showAndWait();
+        newFarmItem = newItem.get();
+    }
+
+    public void createContainer(){
+
+        Dialog<Boolean> newItemDialog = new Dialog();
+        TextField itemNameField = new TextField();
+        TextField itemPriceField = new TextField();
+        TextField itemXField = new TextField();
+        TextField itemYField = new TextField();
+        TextField itemLengthField = new TextField();
+        TextField itemWidthField = new TextField();
+        TextField itemHeightField = new TextField();
+
+        Button acceptButton = new Button("OK");
+        newItemDialog.setResult(true);
+        Text txt = new Text("Enter Item Values");
+        VBox pane = new VBox(10);
+        AtomicReference<FarmItem> newItem = new <FarmItem>AtomicReference();
+
+        newItemDialog.setTitle("New Item");
+
+        acceptButton.setOnAction(e -> {
+            FarmItem values = new FarmItem(
+                    itemNameField.getText(),
+                    Float.parseFloat(itemPriceField.getText()),
+                    Float.parseFloat(itemXField.getText()),
+                    Float.parseFloat(itemYField.getText()),
+                    Float.parseFloat(itemLengthField.getText()),
+                    Float.parseFloat(itemWidthField.getText()),
+                    Float.parseFloat(itemHeightField.getText())
+            );
+            newItem.set(values);
+
+
+            newItemDialog.close();
+        });
+        Font font = Font.font("verdana", FontWeight.MEDIUM, FontPosture.REGULAR, 12);
+        txt.setFont(font);
+        Label nameLabel = new Label("Name");
+        Label priceLabel = new Label("Price");
+        Label xLabel = new Label("X Coordinate");
+        Label yLabel = new Label("Y Coordinate");
+        Label lengthLabel = new Label("Length");
+        Label widthLabel = new Label("Width");
+        Label heightLabel = new Label("Height");
+
+        GridPane fieldPane = new GridPane();
+        fieldPane.add(txt,0,0);
+        fieldPane.add(nameLabel, 0,1);
+        fieldPane.add(priceLabel, 0,2);
+        fieldPane.add(xLabel, 0,3);
+        fieldPane.add(yLabel, 0,4);
+        fieldPane.add(lengthLabel, 0,5);
+        fieldPane.add(widthLabel, 0,6);
+        fieldPane.add(heightLabel, 0,7);
+        fieldPane.add(itemNameField,1,1);
+        fieldPane.add(itemPriceField,1,2);
+        fieldPane.add(itemXField,1,3);
+        fieldPane.add(itemYField,1,4);
+        fieldPane.add(itemLengthField,1,5);
+        fieldPane.add(itemWidthField,1,6);
+        fieldPane.add(itemHeightField,1,7);
+        fieldPane.add(acceptButton, 0,9);
+        pane.getChildren().add(fieldPane);
+        newItemDialog.getDialogPane().setContent(pane);
+        newItemDialog.showAndWait();
         newFarmItem = newItem.get();
     }
     @FXML
@@ -119,19 +192,37 @@ public class DroneDashboardController implements Initializable {
         }
         if (currentAction == "Add Item Container"){
             createItem();
-            itemOptions.getItems().add(newFarmItem);
+            farmObjects.add(newFarmItem);
 
+        }
+        for(FarmObject i: farmObjects){
+            TreeItem node = buildTree(i);
+            rootItem.getChildren().add(node);
         }
 
     }
-
+    public TreeItem buildTree(FarmObject object){
+        TreeItem node = new TreeItem<>();
+        node.setValue(object);
+        if(object instanceof FarmBuilding){
+            for(ItemContainer ic: ((FarmBuilding) object).getItemContainers()){
+                node.getChildren().add(buildTree(ic));
+            }
+            for(Item i: ((FarmBuilding) object).getItems()){
+                TreeItem child = new TreeItem();
+                child.setValue(i);
+                node.getChildren().add(child);
+            }
+        }
+        return node;
+    }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        FarmItem item = new FarmItem("Cow", 250, 10, 10, 10, 10, 10);
         FarmBuilding room = new FarmBuilding("CowContainer", 2000, 10, 10, 10, 10, 10);
-        room.addItem(item);
+        room.addItem(new FarmItem("test",2,2,2,2,2,2));
+        farmObjects.add(room);
         ArrayList optionList = new ArrayList<>();
         optionList.add("Item Root Commands");
         optionList.add("Add Item Container");
@@ -148,11 +239,7 @@ public class DroneDashboardController implements Initializable {
         rootItem.setValue("root");
 
         // Gets each item in the Farm Building instance
-        for(Item i: room.getItems()){
-            TreeItem node = new TreeItem<>();
-            node.setValue(i);
-            rootItem.getChildren().add(node);
-        }
+
 
         // Sets root for Item Tree
         itemTree.setRoot(rootItem);
